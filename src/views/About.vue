f5172faa1a8ddf1382838af83cca5ef1d5efc467
charbo23 <charbo233@qq.com>
2019-09-15 16:32:55 +0800

    按需引入elementUI

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+   <p>Input: {{input}}</p>
+   <el-input v-model="input" placeholder="请输入内容"></el-input>
   </div>
+  
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: ''
+    }
+  }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
+
 module.exports = {
     devServer: {
         port: 8080,
+    },
+    chainWebpack: (config) => {
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('@assets', resolve('src/assets'))
     }
 };
