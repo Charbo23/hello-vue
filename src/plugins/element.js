db30db02edd6786169b118f455c4ef7f037199dd
charbo23 <charbo233@qq.com>
2019-09-17 22:11:09 +0800

    彻底解决打包bug

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import { Button, Input, Table, TableColumn, Loading, Notification } from 'element-ui'
 //不知道为啥build会报错，不过只是不显示打包后大小，不影响结果,初步推测是打包文件过大
 //开启CSS Source Map就好了？？？什么鬼
+//原来是打包的单文件过大报warning但warning信息本身出bug了，调大阈值就好了
 Vue.use(Button);
 Vue.use(Input);
 Vue.use(Table);
--- vue.config.js
+++ vue.config.js
@@ -8,16 +8,16 @@ module.exports = {
     devServer: {
         port: 8080,
     },
-
     chainWebpack: (config) => {
         config.resolve.alias
             .set('@', resolve('src'))
             .set('@assets', resolve('src/assets'))
-            .set('@style', resolve('src/style'))
-
+            .set('@style', resolve('src/style'));
+        config.performance
+            .set('maxAssetSize', 1024 * 1024)
+            .set('maxEntrypointSize', 1024 * 1024);
     },
-
-    css: {
-        sourceMap: true
-    }
+    // css: {
+    //     sourceMap: true
+    // }
 };
