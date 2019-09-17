eb023fa8c8c448e0f7740df035761117f3d2c50b
charbo23 <charbo233@qq.com>
2019-09-17 17:43:24 +0800

    使用mock做小demo

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
-import { Button,Input} from 'element-ui'
+import { Button, Input, Table, TableColumn, Loading, Notification } from 'element-ui'
 
 Vue.use(Button);
 Vue.use(Input);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Loading.directive);
+Vue.prototype.$notify = Notification;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,58 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button @click="fetchData(false)" v-loading.fullscreen.lock="initLoading">获取数据</el-button>
     <p>Input: {{input}}</p>
     <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-table :data="tableData" style="width: 100%" v-loading="loading">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
+  name: "about",
   data() {
     return {
-      input: ""
+      input: "",
+      tableData: [
+        {
+          date: "2019-09-17",
+          name: "Charbo",
+          address: "湖州师范学院"
+        }
+      ],
+      loading: false,
+      initLoading: false
     };
+  },
+  methods: {
+    fetchData: function(isInit = false) {
+      if (this.loading) {
+        return;
+      }
+      isInit ? (this.initLoading = true) : (this.loading = true);
+
+      axios.get("/mock.json").then(response => {
+        setTimeout(() => {
+          this.tableData = response.data.tableData;
+          this.loading = false;
+          this.initLoading = false;
+          this.$notify({
+            title: "成功",
+            message: "数据获取成功",
+            type: "success"
+          });
+        }, 2000);
+      });
+    }
+  },
+  created: function() {
+    this.fetchData(true);
   }
 };
 </script>
