1d0528a1d7e4f55a37b52bd7da3cfb2f6bc2d9f2
charbo23 <charbo233@qq.com>
2019-09-17 18:58:19 +0800

    bugfix

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import { Button, Input, Table, TableColumn, Loading, Notification } from 'element-ui'
-
+//不知道为啥build会报错，不过只是不显示打包后大小，不影响结果
 Vue.use(Button);
 Vue.use(Input);
 Vue.use(Table);
