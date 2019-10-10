4101dd91ad0bf61d3ae8cab405c75084f0e85896
charbo23 <charbo233@qq.com>
2019-10-10 08:49:32 +0800

    更新游戏逻辑，加入键盘事件

--- src/views/Bag.vue
+++ src/views/Bag.vue
@@ -7,7 +7,12 @@
     <div class="bag-control">
       <div class="btn-group">
         <el-button-group>
-          <el-button icon="el-icon-minus" @click="decrease" :disabled="isBurst">放气</el-button>
+          <el-button
+            icon="el-icon-minus"
+            @keyup.left="decrease"
+            @click="decrease"
+            :disabled="isBurst"
+          >放气</el-button>
           <el-button icon="el-icon-plus" @click="increase" :disabled="isBurst">打气</el-button>
         </el-button-group>
       </div>
@@ -24,6 +29,27 @@
 import _ from "lodash";
 export default {
   name: "Bag",
+  mounted() {
+    document.onkeydown = e => {
+      let key = window.event.keyCode;
+      switch (key) {
+        case 37:
+          window.event.preventDefault();
+          this.decrease();
+          break;
+        case 39:
+          window.event.preventDefault();
+          this.increase();
+          break;
+        case 32:
+          window.event.preventDefault();
+          this.restart();
+          break;
+        default:
+          break;
+      }
+    };
+  },
   data() {
     return {
       percentage: 0,
@@ -46,34 +72,35 @@ export default {
   },
   watch: {
     isBurst: function(cur) {
-      //   console.log(`pre: ${pre}, cur: ${cur}`);
       if (cur === true) {
-        this.$confirm("是否重新开始游戏?", "提示", {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning",
-          closeOnClickModal: false
-        })
-          .then(() => {
-            this.restart();
-          })
-          .catch(() => {
-            // this.$message({
-            //   message: "游戏结束",
-            //   type: "warning"
-            // });
-            this.$notify({
-              title: "提示",
-              message: "游戏结束",
-              type: "warning"
-            });
-          });
+        // this.$confirm("是否重新开始游戏?", "提示", {
+        //   confirmButtonText: "确定",
+        //   cancelButtonText: "取消",
+        //   type: "warning",
+        //   closeOnClickModal: false
+        // })
+        //   .then(() => {
+        //     this.restart();
+        //   })
+        //   .catch(() => {
+
+        //     this.$notify({
+        //       title: "提示",
+        //       message: "游戏结束",
+        //       type: "warning"
+        //     });
+        //   });
+        this.$message({
+          message: "游戏结束",
+          type: "warning"
+        });
       }
     }
   },
   methods: {
     increase: _.throttle(
       function() {
+        if (this.isBurst) return;
         this.percentage + this.diff >= 100
           ? (this.percentage = 100)
           : (this.percentage += this.diff);
@@ -86,6 +113,7 @@ export default {
     ),
     decrease: _.throttle(
       function() {
+        if (this.isBurst) return;
         this.percentage - this.diff < 0
           ? (this.percentage = 0)
           : (this.percentage -= this.diff);
@@ -97,14 +125,25 @@ export default {
       }
     ),
     restart: function() {
-      this.percentage = 0;
+      if (this.isBurst) this.percentage = 0;
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.bag{
+@keyframes bugScale {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.3);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+.bag {
   padding-top: 50px;
 }
 .bag-img {
@@ -114,9 +153,11 @@ export default {
   width: 100px;
   height: 250px;
   margin: 0 auto;
+  transition: all 0.3s;
   &.burst {
     background: url("~@assets/bag-burst.png") center no-repeat;
     background-size: 100%;
+    animation: bugScale 0.5s;
   }
 }
 .btn-group {
@@ -127,7 +168,7 @@ export default {
 .bag-health {
   width: 50%;
   margin: 0 auto;
-  margin-top: 20px;
+  margin-top: 40px;
 }
 .bag-control {
   margin-top: 20px;
